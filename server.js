3a3e75a220d2885cd26493609c766981ea2a766d
lndsilva <lndsilva@hotmail.com>
2021-09-21 15:13:51 -0300

    Configuração do server

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middewares = jsonServer.defaults();
+
+const port = process.env.PORT || 8080;
+
+server.use(middewares);
+server.use(router);
+server.listen(port, ()=> {
+    console.log(`json-server is running in ${port}`);
+}
+
+);
